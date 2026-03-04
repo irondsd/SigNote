92fd951e183cbdc054e4920be3c8624151cd499a
Konstantin Mednikov <irondsd@gmail.com>
2026-03-04 08:26:22 -0300

    fix build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import { attachDatabasePool } from '@vercel/functions';
 import NextAuth from 'next-auth';
+import { NextRequest } from 'next/server';
 
 import { authOptions } from '@/utils/auth';
 import { getMongoClientFromMongoose } from '@/utils/mongoose';
@@ -8,14 +9,14 @@ export const runtime = 'nodejs';
 
 const handler = NextAuth(authOptions);
 
-export async function GET(request: Request, context: { params: { nextauth: string[] } }) {
+export async function GET(request: NextRequest, context: { params: Promise<{ nextauth: string[] }> }) {
   const client = await getMongoClientFromMongoose();
   attachDatabasePool(client);
 
   return handler(request, context);
 }
 
-export async function POST(request: Request, context: { params: { nextauth: string[] } }) {
+export async function POST(request: NextRequest, context: { params: Promise<{ nextauth: string[] }> }) {
   const client = await getMongoClientFromMongoose();
   attachDatabasePool(client);
 
