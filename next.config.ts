968535b88f00fda5fac890728d2772527b155263
Konstantin Mednikov <irondsd@gmail.com>
2026-03-22 17:17:32 -0300

    try to fix preview auth

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    // For Vercel deployments, ensure NEXTAUTH_URL is set to the correct URL
+    NEXTAUTH_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXTAUTH_URL, // fallback to your .env for local dev
+  },
 };
 
 export default nextConfig;
