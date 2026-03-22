bca33e1f0ee95659423e9cf4b5aa76d8621b77c1
Konstantin Mednikov <irondsd@gmail.com>
2026-03-22 15:23:03 -0300

    note colors remove unused and todo

--- src/config/noteColors.ts
+++ src/config/noteColors.ts
@@ -9,3 +9,11 @@ export const SWITCH_COLORS: Record<NoteColor, string> = {
   clay: '#E6B8A2',
   gray: '#BDBDBD',
 };
+
+// todo: add more and
+// blue light #AECCDC dark #294255
+// green light #E2F6D3 dark #254D3B
+// sand light #FFF8B8 dark #7C4A02
+// clay light #E9E3D4 dark #4B443B
+// peach light #F39F76 dark #692B16
+// purple light #D3BFDB dark #472E5B
