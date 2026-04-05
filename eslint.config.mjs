95a12952537aa057295934d278b27c59bc840773
Konstantin Mednikov <irondsd@gmail.com>
2026-04-05 13:22:33 -0300

    serwist, sw install

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
     'tests/provider/mock-provider.js',
+    'public/sw.js',
   ]),
 ]);
 
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
+import withSerwistInit from '@serwist/next';
 import type { NextConfig } from 'next';
 
+const withSerwist = withSerwistInit({
+  swSrc: 'src/sw.ts',
+  swDest: 'public/sw.js',
+  disable: process.env.NODE_ENV !== 'production',
+});
+
 const nextConfig: NextConfig = {
+  turbopack: {},
   async headers() {
     return [
       {
@@ -16,4 +24,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
